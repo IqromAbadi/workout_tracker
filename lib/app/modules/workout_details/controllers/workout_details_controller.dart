49a3d23f52f58d960f2bbc4e3c6f403a5935d8f0
Iqrom Abadi <iqromabadi15@gmail.com>
2024-09-22 22:48:48 +0700

    need items

--- lib/app/modules/workout_details/controllers/workout_details_controller.dart
+++ lib/app/modules/workout_details/controllers/workout_details_controller.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:workout_tracker/app/data/items.dart';
 
 class WorkoutDetailsController extends GetxController {
+//MENGAMBIL DATA BERDASARKAN ITEM YANG DIPILIH DARI PAGE WORKOUT TRACKER
   String workoutName = '';
   String workoutDescription = '';
 
@@ -12,5 +14,33 @@ class WorkoutDetailsController extends GetxController {
     workoutDescription =
         Get.arguments['subtitle'] ?? 'Default Workout Description';
     print('Workout: $workoutName, $workoutDescription');
+  }
+
+  //MENGAMBIL DATA ITEM YANG DIBUTUHKAN
+  var itemneedlist = <ItemNeed>[].obs;
+
+  void fetchitems() async {
+    await Future.delayed(const Duration(seconds: 0));
+    var fetcheditems = [
+      ItemNeed(
+        title: "Barbel",
+        imageUrl: "assets/images/burble.png",
+      ),
+      ItemNeed(
+        title: "Skiping Rope",
+        imageUrl: "assets/images/skipingrope.png",
+      ),
+      ItemNeed(
+        title: "Bottele Liter",
+        imageUrl: "assets/images/bottle.png",
+      ),
+    ];
+    itemneedlist.value = itemneedlist;
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+    fetchitems();
   }
 }
