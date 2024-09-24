4eb8d7fcaabe9169522f4aa9be26bb56c2f6c429
Iqrom Abadi <iqromabadi15@gmail.com>
2024-09-24 15:32:22 +0700

    workout details done

---
+++ lib/app/data/set2.dart
@@ -0,0 +1,11 @@
+class Set2 {
+  final String name;
+  final String description;
+  final String image;
+
+  Set2({
+    required this.name,
+    required this.description,
+    required this.image,
+  });
+}
